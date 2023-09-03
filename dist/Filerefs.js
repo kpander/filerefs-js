eca1b296e2477abf27ab0376f6c5e453b470b349
Kendall Anderson <kpander@invisiblethreads.com>
2023-09-03 07:46:08 -0400

    Initial poc build of Filerefs

---
+++ dist/Filerefs.js
@@ -0,0 +1,4 @@
+/* https://github.com/kpander/filerefs-js */
+/* dist/Filerefs.js v0.0.1 Sun Sep 03 2023 07:45:06 GMT-0400 (Eastern Daylight Saving Time) */
+
+"use strict";const fs=require("fs"),path=require("path");module.exports=class a{static getFilerefs(c,i={}){if("string"!=typeof c)return!1;i.basePath=i.basePath||"",i.tags=i.tags||a.TAGS;let h={};return Object.keys(i.tags).forEach(t=>{var e=i.tags[t],t=new RegExp(`(<${t}[^>]+${e}=")(?!http)(.*?)(")`,"ig");for(const r of c.matchAll(t)){var s=r[0];h[s]=a._getRef(i.basePath,r[1],r[2],r[3])}}),h}static _getRef(t,e,s,r){t=path.join(t,s),t=new URL("http://test.com"+t);return{pre:e,relative:s,post:r,relativeBase:s.replace(t.search,""),relativeParams:t.search,absolute:!!fs.existsSync(t.pathname)&&t.pathname}}static get TAGS(){return{link:"href",script:"src",img:"src",source:"srcset",audio:"src",video:"src",track:"src"}}};
